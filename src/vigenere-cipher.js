e6215c41fa52cd6aee4050f317a61391ef0e60bc
Oksana <5oks5@mail.ru>
2023-04-14 20:31:18 +0400

    feat: add ALL SOLUTION

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -29,7 +29,70 @@ class VigenereCipheringMachine {
 if(string === undefined || key === undefined) {
   throw new Error('Incorrect arguments!');
 }
-let alphabetArray =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let notEncrypt = '1234567890!@#$(),./|*-&^%: ';
+let alphabetArray = alphabet.split('');
+let notEncryptArray = notEncrypt.split('');
+
+let stringArray = string.toUpperCase().split('');
+let keyArray = key.toUpperCase().split('');
+
+let stringArrayToShifr = [];
+let immutableElements = [];
+stringArray.forEach(item => {
+if(!notEncryptArray.includes(item)) {
+stringArrayToShifr.push(item);
+} else {
+immutableElements.push(item);
+}
+})
+
+let indexStringShifr = stringArrayToShifr.map(item => alphabetArray.indexOf(item));
+let indexKeyShifr = keyArray.map(item => alphabetArray.indexOf(item));
+
+//console.log(immutableElements);
+//console.log(indexKeyShifr);
+//console.log(indexStringShifr);
+
+let newKey = [];
+while(newKey.length < indexStringShifr.length)
+{
+newKey.push(...indexKeyShifr)
+}
+newKey = newKey.slice(0, indexStringShifr.length);
+
+//console.log(newKey);
+let arraySum = [];
+for (let i=0; i < newKey.length; i++) {
+arraySum.push(newKey[i]+indexStringShifr[i]);
+}
+//console.log(arraySum);
+arraySum = arraySum.map(item => {
+if(item >= 26) {
+return item%26
+} else return item
+})
+console.log(arraySum);
+let result = [];
+arraySum.forEach(item => {
+result.push(alphabetArray[item]);
+}
+)
+console.log(result);
+  let allResalt = []
+stringArray.forEach(item => {
+  if(notEncryptArray.includes(item)) {
+    allResalt.push(item)
+  } else {
+    let needSimbol = result.shift();
+    allResalt.push(needSimbol)
+  }
+})
+ console.log(allResalt);
+ if(this.condition === false) {
+  return allResalt.reverse().join('');
+ }
+return allResalt.join('');
   }
 
 
@@ -38,6 +101,70 @@ let alphabetArray =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
 if(string === undefined || key === undefined) {
   throw new Error('Incorrect arguments!');
 }
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let notEncrypt = '1234567890!@#$(),./|*-&^%: ';
+let alphabetArray = alphabet.split('');
+let notEncryptArray = notEncrypt.split('');
+
+let stringArray = string.toUpperCase().split('');
+let keyArray = key.toUpperCase().split('');
+
+let stringArrayToShifr = [];
+stringArray.forEach(item => {
+if(!notEncryptArray.includes(item)) {
+stringArrayToShifr.push(item);
+} 
+})
+
+  console.log(stringArrayToShifr)
+
+let indexStringShifr = stringArrayToShifr.map(item => alphabetArray.indexOf(item));
+let indexKeyShifr = keyArray.map(item => alphabetArray.indexOf(item));
+
+//console.log(immutableElements);
+//console.log(indexKeyShifr);
+//console.log(indexStringShifr);
+
+let newKey = [];
+while(newKey.length < indexStringShifr.length)
+{
+newKey.push(...indexKeyShifr)
+}
+newKey = newKey.slice(0, indexStringShifr.length);
+
+//console.log(newKey);
+let arrayDifference = [];
+for (let i=0; i < newKey.length; i++) {
+arrayDifference.push(indexStringShifr[i] - newKey[i]);
+}
+console.log(arrayDifference);
+arrayDifference = arrayDifference.map(item => {
+if(item < 0) {
+return item + 26;
+} else return item
+})
+console.log(arrayDifference);
+let result = [];
+arrayDifference.forEach(item => {
+result.push(alphabetArray[item]);
+}
+)
+console.log(result);
+  let allResalt = []
+stringArray.forEach(item => {
+  if(notEncryptArray.includes(item)) {
+    allResalt.push(item)
+  } else {
+    let needSimbol = result.shift();
+    allResalt.push(needSimbol)
+  }
+})
+ console.log(allResalt);
+ if(this.condition === false) {
+  return allResalt.reverse().join('');
+ }
+return allResalt.join('');
+
   }
 }
 
